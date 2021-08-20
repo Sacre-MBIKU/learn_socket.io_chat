c991fd6473f4dba866c54a0f27421cc22b915d2c
SacréMbiku <sajombiku@gmail.com>
2021-08-20 13:01:53 +0100

    feature : handle socket io

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const http = require("http");
+const { Server } = require("socket.io");
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT || 3009;
 
 dotenv.config({ path: "./config.env" });
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("chat message", (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on("disconnect", () => console.log("a user has been disconnected"));
 });
 
 server.listen(PORT, () => console.log(`Server is listen on ${PORT}`));
